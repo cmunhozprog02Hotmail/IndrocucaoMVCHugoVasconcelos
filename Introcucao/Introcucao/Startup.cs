475190e861f485027ec6330686a863c4d0d4a98a
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-04-10 23:23:16 -0300

    initial

---
+++ Introcucao/Introcucao/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Introcucao.Startup))]
+namespace Introcucao
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
